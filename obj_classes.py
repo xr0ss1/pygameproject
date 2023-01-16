b242cf7329ec8ad0338bdbc000db5eb339674e4d
xr0ss1 <122720355+xr0ss1@users.noreply.github.com>
2023-01-17 03:36:48 +0500

    hud+camera
    hud+camera

--- obj_classes.py
+++ obj_classes.py
@@ -2,8 +2,8 @@ import pygame
 import math
 class Player(pygame.sprite.Sprite):
 	def create_anim(self,filename,n,ind):
-		for i in range(1,n):
-			self.anim_list[ind].append(pygame.image.load(filename+str(i+1)+'.bmp').convert_alpha())
+		for i in range(0,n):
+			self.anim_list[ind].append(pygame.image.load(filename+str(i+1)+'.png').convert_alpha())
 
 			
 			
@@ -18,47 +18,98 @@ class Player(pygame.sprite.Sprite):
 				self.max_img=len(self.images)
 			print(ind)
 			
+	def init_guns(self):
+			self.guns=[]
+			self.guns.append([pygame.image.load('sprites/pl_pistol.png').convert_alpha(),20,100,7,300]		)
 			
 	def __init__(self,x):
 		self.move_check=[0,0,0,0]
 		self.anim_list={'move':[],'stand':[]}
-		self.create_anim('sprites/s_pl_move',6,'stand')
-		self.create_anim('sprites/s_pl_move',6,'move')
+		self.create_anim('sprites/pl_walk',6,'stand')
+		self.create_anim('sprites/pl_walk',6,'move')
 		self.cur_anim='--'
 		self.change_anim('stand')
+		self.init_guns()
+		self.rect = self.images[0].get_rect(center=(320,160))
 		
-		self.rect = self.images[0].get_rect()
-		self.rect.x=640
-		self.rect.y=320
-		self.rect.move_ip(32,32)
 		
+		self.hp=100
 		self.speed = 0
 		self.last_sd=-1
-		self.max_speed=2
+		self.max_speed=1
 		self.cur_img=0
 		self.img_cnt=0
-		self.img_speed=60
+		self.img_speed=120
 		self.max_img=len(self.images)
 		
 		self.deg=0
-		
+		self.legs_deg=0
 		self.r_x=0
 		self.r_y=0
 		
 		self.image=self.images[self.cur_img]
-		self.rot_image=pygame.transform.rotate(self.image,0)
 		
+		
+		self.cur_gun=0
+		
+		self.gun_image=self.guns[self.cur_gun][0]
+		self.leg_image=pygame.transform.rotate(self.image,self.legs_deg)
+		self.gun_rect=self.gun_image.get_rect(center=self.rect.center)
+		self.legs_rect=self.gun_image.get_rect(center=self.rect.center)
+		self.rot_image=pygame.transform.rotate(self.gun_image,0)
+		
+		self.can_shoot=1
+		self.gun_timer=self.guns[self.cur_gun][2]
+		self.max_ammo=self.guns[self.cur_gun][3]
+		self.cur_ammo=self.max_ammo
+		self.reload_timer=self.guns[self.cur_gun][4]
 	def rotate(self):
 		mouse_x,mouse_y=pygame.mouse.get_pos()
 		rel_x,rel_y=mouse_x-self.rect.x,mouse_y-self.rect.y
 		angle=(180/math.pi)*(-math.atan2(rel_y,rel_x))
-		self.rot_image=pygame.transform.rotate(self.image,angle)
+		if angle<0:
+			angle=180+(180+angle)
+		legs_deg=self.legs_deg
+		if (legs_deg<315)and(legs_deg>45):
+			if (abs(legs_deg-angle))<=90:
+				self.rot_image=pygame.transform.rotate(self.gun_image,angle)
+		else:
+			if legs_deg==315:
+				if (angle>=225 and angle<=360)or(angle<=45 and angle>=0):
+					self.rot_image=pygame.transform.rotate(self.gun_image,angle)
+			if legs_deg==45:
+				if (angle>=315 and angle<=360)or(angle<=135 and angle>=0):
+					self.rot_image=pygame.transform.rotate(self.gun_image,angle)
+			if legs_deg==0:
+				if (angle>=270 and angle<=360)or(angle<=90 and angle>=0):
+					self.rot_image=pygame.transform.rotate(self.gun_image,angle)
+		self.rot_image=pygame.transform.rotate(self.gun_image,angle)
+		self.leg_image=pygame.transform.rotate(self.image,self.legs_deg)
 		
 	def move(self,*args):
 		if abs(self.speed)<abs(self.max_speed):
 			self.speed+=.1
 		self.move_check[args[0]]=1
 		self.change_anim('move')
+	
+	def shoot_timer(self):
+		if self.can_shoot==0:
+			self.gun_timer-=1
+			
+			if self.gun_timer<=0:
+				print('shoot')
+				if self.cur_ammo>0:
+					self.can_shoot=1
+				
+				self.gun_timer=self.guns[self.cur_gun][2]
+			if self.cur_ammo<=0:
+				print(str(self.reload_timer))
+				self.reload_timer-=1
+				if self.reload_timer<=0:
+					print('reloaded')
+					self.reload_timer=self.guns[self.cur_gun][4]
+					self.can_shoot=1
+					self.cur_ammo=self.guns[self.cur_gun][3]
 		
 	def anim_check(self,k_p):
 		self.img_cnt+=1
@@ -71,14 +122,17 @@ class Player(pygame.sprite.Sprite):
 			self.change_anim('stand')
 	def draw(self,par):
 		#self.image.rect.center=(-32,-32)
-		self.rect=self.rot_image.get_rect(center=self.rect.center)
-		par.blit(self.rot_image,self.rect)
+		self.gun_rect=self.rot_image.get_rect(center=self.rect.center)
+		self.legs_rect=self.leg_image.get_rect(center=self.rect.center)
+		#par.blit(self.leg_image,self.legs_rect)
+		par.blit(self.rot_image,self.gun_rect)
 		rad=float(self.deg/(180/math.pi))
 		dx=self.rect.x+int(math.cos(rad)*30)
 		dy=self.rect.y+int(math.sin(rad)*30)
-		pygame.draw.line(par, (255,255,255), [self.rect.x, self.rect.y],[dx,dy], 3)
+		#pygame.draw.line(par, (255,255,255), [],[dx,dy], 3)
 	
-	def update(self,k_p):
+	def update(self,k_p,cam):
+		self.shoot_timer()
 		self.image=self.images[self.cur_img]
 		self.anim_check(k_p)
 		self.rotate()
@@ -95,18 +149,37 @@ class Player(pygame.sprite.Sprite):
 			if i==1 :
 				cnt+=1
 		d_sp=self.speed
+		bems=self.move_check
 		if cnt>1:
 			d_sp=self.speed/math.sin(math.radians(45))
+			
+			if bems[0]==1 and bems[1]==1:
+				self.legs_deg=135
+			if bems[1]==1 and bems[2]==1:
+				self.legs_deg=45
+			if bems[2]==1 and bems[3]==1:
+				self.legs_deg=315
+			if bems[3]==1 and bems[0]==1:
+				self.legs_deg=225
+		else:
+			if bems[0]==1:
+				self.legs_deg=180
+			if bems[1]==1:
+				self.legs_deg=90
+			if bems[2]==1:
+				self.legs_deg=0
+			if bems[3]==1:
+				self.legs_deg=270
 		for i in range(len(self.move_check)):
 			if self.move_check[i]==1:
 				if i==0:
-					self.rect.x-=d_sp
+					cam.move(d_sp,0)
 				if i==1:
-					self.rect.y-=d_sp
+					cam.move(0,d_sp)
 				if i==2:
-					self.rect.x+=d_sp
+					cam.move(-d_sp,0)
 				if i==3:
-					self.rect.y+=d_sp
+					cam.move(0,-d_sp)
 			self.move_check[i]=0
 			
 		
@@ -129,4 +202,30 @@ class Bullet(pygame.sprite.Sprite):
 	def update(self):
 		self.rect.x+=self.dx
 		self.rect.y+=self.dy
+
+class Camera(pygame.sprite.Sprite):
+	def __init__(self,x,y):
+		self.rect = pygame.Rect(x,y,320,160)
+	def move(self,x,y):
+		self.rect[0]+=x
+		self.rect[1]+=y
+class zombie(pygame.sprite.Sprite):
+	pass
+
+class HUD(pygame.sprite.Sprite):
+	def __init__(self):
+		self.bul_img=pygame.image.load('sprites/hud_bullet.png').convert_alpha()
+		self.ebul_img=pygame.image.load('sprites/hud_bullet_empty.png').convert_alpha()
+		self.bul_list=[]
+	def init_bul(self,par,hero):
+		self.bul_list=[]
+		for i in range(hero.max_ammo):
+			if hero.cur_ammo>=i+1:
+				img=self.bul_img
+			else:
+				img=self.ebul_img
+			img_rect=pygame.Rect(0+(i)*15,320-25,10,20)
+			par.blit(img,img_rect)
 		
+
+
--- pygame1.py
+++ pygame1.py
@@ -1,41 +1,52 @@
 import pygame
 import math
 pygame.font.init()
-W=1200
-H=640
-from obj_classes import Player,Bullet
+W=640
+H=320
+FPS=30
+
+mouse_down=0
+from obj_classes import Player,Bullet,Camera,HUD
 sc=pygame.display.set_mode((W,H))
 pygame.display.set_caption('бимбим бамбам')
 #pygame.display.set_icon(pygame.image.load('name'))
 pygame.draw.rect(sc,(255,255,255),(10,10,50,100))
 GEnable=True
 key_events={'key_left':False,'key_right':False,'key_up':False,'key_down':False}#задаем проверки которые используем
-key_events_check={'key_left':pygame.K_LEFT,'key_right':pygame.K_RIGHT,'key_up':pygame.K_UP,'key_down':pygame.K_DOWN} #назначаем клавиши
+key_events_check={'key_left':pygame.K_a,'key_right':pygame.K_d,'key_up':pygame.K_w,'key_down':pygame.K_s} #назначаем клавиши
 
 tt=pygame.font.Font(None, 12)
 text_test=tt.render('1232',True,(255,255,255))
 
-background=Surface(W*2,H*2)
+background=pygame.image.load('sprites/background.png').convert_alpha()
+back_rect=background.get_rect(topleft=(0,0))
 
 bul_list=[]
-camera=[0,0]
+cam=Camera(0,0)
+hud=HUD()
 hero=Player(W//2)
 def check_events():
 	if key_events['key_left']:
 		hero.move(0)
 		
+		
 	if key_events['key_right']:
 		hero.move(2)
+		
 		
 	if key_events['key_up']:
 		hero.move(1)
 		
 	if key_events['key_down']:
 		hero.move(3)
+		
 while GEnable:
 	sc.fill((0,0,0))
+	sc.blit(background,(back_rect.x+cam.rect.x,back_rect.y+cam.rect.y))
 	hero.draw(sc)
-	text_test=tt.render(str(hero.speed),True,(255,255,255))
+	hud.init_bul(sc,hero)
+	mx,my=pygame.mouse.get_pos()
+	text_test=tt.render('x= '+str(mx)+' y= '+str(my),True,(255,255,255))
 	if len(bul_list)>0:
 		for i in bul_list:
 			i.update()
@@ -55,12 +66,18 @@ while GEnable:
 				if event.key==key_events_check[i]:
 					key_events[i]=False
 		if event.type==pygame.MOUSEBUTTONDOWN:
-			bul_list.append(Bullet(1,hero.rect.centerx,hero.rect.centery,event.pos[0],event.pos[1]))
+			mouse_down=1
+		if event.type==pygame.MOUSEBUTTONUP:
+			mouse_down=0
 		if event.type==pygame.MOUSEMOTION:
 			pass
-			
+	if mouse_down==1:
+		if hero.can_shoot==1:
+			bul_list.append(Bullet(1,hero.rect.centerx,hero.rect.centery,event.pos[0],event.pos[1]))		
+			hero.can_shoot=0
+			hero.cur_ammo-=1
 	check_events()
-	hero.update(key_events)
+	hero.update(key_events,cam)
 	
 			
 	
